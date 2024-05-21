bf0997fb0238fc8eea18385e15107865bdea5ae2
KobiManuel <emmakobi91@gmail.com>
2024-05-21 23:54:32 +0100

    feat: fixed pseudoclass error

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <main>
       <Sidebar />
-      <div className="main-content w-[calc(100% - 269px)] ml-[269px]  overflow-y-auto flex flex-col items-center">
+      <div className="main-content w-[calc(100% - 269px)] ml-[269px]  overflow-y-auto overflow-x-hidden flex flex-col items-center">
         <Header />
         <Angels />
       </div>
